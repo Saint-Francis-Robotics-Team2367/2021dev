30d698bcae3c66cd8ca1f9f5e8066a965779e1f9
aditya2604 <aditya26042005@gmail.com>
2021-09-25 12:20:18 -0700

    tshirt cannon code

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,97 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+#include <frc/smartdashboard/SmartDashboard.h>
+
+void Robot::RobotInit() {
+  m_leftLeadMotor->RestoreFactoryDefaults();
+  m_rightLeadMotor->RestoreFactoryDefaults();
+  m_leftFollowMotor->RestoreFactoryDefaults();
+  m_rightFollowMotor->RestoreFactoryDefaults();
+
+  m_leftEncoder.SetPosition(0);
+  m_rightEncoder.SetPosition(0);
+
+  m_leftLeadMotor->SetInverted(true);
+  m_leftFollowMotor->Follow(*m_leftLeadMotor, false);
+  m_rightLeadMotor->SetInverted(false);
+  m_rightFollowMotor->Follow(*m_rightLeadMotor, false);
+}
+void Robot::RobotPeriodic() {
+  frc::SmartDashboard::PutNumber("left y: ", -(m_stick->GetRawAxis(1)));
+  frc::SmartDashboard::PutNumber("right x: ", m_stick->GetRawAxis(4));
+}
+ 
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {
+  m_leftEncoder.SetPosition(0);
+  m_rightEncoder.SetPosition(0);
+  compressor = new frc::Spark(1);
+  pressed_button_pressure = true;
+  valve.Set(false);
+}
+void Robot::TeleopPeriodic() {
+  left_y = m_stick->GetRawAxis(1);
+  right_x = m_stick->GetRawAxis(4);
+
+  m_robotDrive->ArcadeDrive(-left_y, right_x);
+
+  analog_input->GetVoltage();
+  frc::SmartDashboard::PutNumber("analogInput", analog_input->GetVoltage());
+
+  var_input = frc::SmartDashboard::GetNumber("varInput", 1);
+  frc::SmartDashboard::PutNumber("varInput", var_input);
+
+  maxPSI = frc::SmartDashboard::GetNumber("maxPSI", 82);
+  frc::SmartDashboard::PutNumber("maxPSI", maxPSI);
+
+  PSI = (analog_input->GetVoltage()) * 100 + 10; // transfer function
+  if (m_stick->GetRawButtonPressed(1)) {
+    valve.Set(false);
+    pressed_button_pressure = true;
+    reached_max_pressure = false;
+    frc::SmartDashboard::PutBoolean("triggerpress", true);
+    frc::SmartDashboard::PutBoolean("valve", false);
+  }
+
+  if ((m_stick->GetRawButtonPressed(2)) && (reached_max_pressure)) {
+    valve.Set(true);
+    frc::SmartDashboard::PutBoolean("valve", true);
+  }
+
+  if (m_stick->GetRawButtonPressed(3)) {
+    valve.Set(false);
+    frc::SmartDashboard::PutBoolean("valve", false);
+  }
+
+  if ((!reached_max_pressure) && (pressed_button_pressure)) {
+    if (PSI < maxPSI) {
+      frc::SmartDashboard::PutNumber("currPSI", PSI);
+      compressor->Set(1);
+    } else {
+      compressor->Set(0);
+      reached_max_pressure = true;
+      pressed_button_pressure = false; 
+      frc::SmartDashboard::PutBoolean("triggerpress", false);
+    }
+  }
+  
+  // if button pressed,
+  // include if statement
+}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ src/main/include/Robot.h
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc/TimedRobot.h>
+#include <frc/Joystick.h>
+#include <frc/AnalogInput.h>
+#include <frc/Spark.h>
+#include "SFDrive.h"
+#include <frc/Solenoid.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+
+  void TestInit() override;
+  void TestPeriodic() override;
+
+  static const int leftLeadDeviceID = 12; // 12
+  static const int leftFollowDeviceID = 13;
+  static const int rightLeadDeviceID = 15; // 15
+  static const int rightFollowDeviceID = 14;
+
+  double left_y = 0.0;
+  double right_x = 0.0;
+
+  int maxPSI;
+  float PSI;
+  float var_input;
+  bool reached_max_pressure = false;
+  bool pressed_button_pressure = true;
+
+  frc::AnalogInput * analog_input = new frc::AnalogInput(1);
+
+  frc::Spark *compressor;
+
+  rev::CANSparkMax* m_leftLeadMotor = new rev::CANSparkMax(leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* m_rightLeadMotor = new rev::CANSparkMax(rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* m_leftFollowMotor = new rev::CANSparkMax(leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* m_rightFollowMotor = new rev::CANSparkMax(rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+
+  rev::CANEncoder m_leftEncoder = m_leftLeadMotor->GetEncoder(rev::CANEncoder::EncoderType::kHallSensor, 42);
+  rev::CANEncoder m_rightEncoder = m_rightLeadMotor->GetEncoder(rev::CANEncoder::EncoderType::kHallSensor, 42);
+
+  frc::Joystick* m_stick = new frc::Joystick{0};
+
+  SFDrive* m_robotDrive = new SFDrive(m_leftLeadMotor, m_rightLeadMotor);
+
+  frc::Solenoid valve{0};
+
+};
