6b362235e05f4d51edaefd09007317efd6955d71
Nitish Gourishetty <59469935+NitishGourishetty@users.noreply.github.com>
2021-10-11 15:31:11 -0700

    Works For Forward and Backwards

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -24,12 +24,12 @@ void Robot::RobotPeriodic()
   //frc::SmartDashboard::PutNumber("x", stick->GetRawAxis(4));
   //frc::SmartDashboard::PutNumber("y ", -stick->GetRawAxis(1));
   frc::SmartDashboard::PutNumber("current velocity", currentVelocity);
-  frc::SmartDashboard::PutNumber("current position", currentPosition);
+  //frc::SmartDashboard::PutNumber("current position", currentPosition);
   frc::SmartDashboard::PutNumber("Right Encoder", m_rightEncoder.GetPosition());
   frc::SmartDashboard::PutNumber("Left Encoder", m_leftEncoder.GetPosition());
-  frc::SmartDashboard::PutNumber("positionTotal", positionTotal);
   frc::SmartDashboard::PutNumber("current velocity", currentVelocity);
-  frc::SmartDashboard::PutNumber("current position", currentPosition);
+  frc::SmartDashboard::PutNumber("Conversion", Robot::convertDistanceToTicks(1));
+  //frc::SmartDashboard::PutNumber("current position", currentPosition);
 }
 
 void Robot::AutonomousInit()
@@ -37,10 +37,10 @@ void Robot::AutonomousInit()
 
   //wait so if it doesnt show me the rest of the values it gives, its probabl the PID
   //double m_P = 0.05, m_I = 0.05, m_D = 0.1, kMaxOutput = 0.25, kMinOutput = -0.25;
-  //m_P starts at 0.0005, but then is changed in periodic
-  double m_P = 0.0005, m_I = 0.000, m_D = 0.0, kMaxOutput = 0.25, kMinOutput = -0.25;
+  //m_P can start at 0.0005, but then is changed in periodic
+  double m_P = 0.05, m_I = 0.000, m_D = 0.0, kMaxOutput = 0.25, kMinOutput = -0.25;
   frc::SmartDashboard::PutNumber("Pd", m_P);
-  frc::SmartDashboard::PutNumber("Total Position", 1);
+  frc::SmartDashboard::PutNumber("Total Position", 6);
 
   //or do I set this to 0
   //Set feet here
@@ -67,7 +67,7 @@ void Robot::AutonomousInit()
 void Robot::AutonomousPeriodic() {
   //Making it so you can manually set m_p and positionTotal: m_p is essential with PID, change by an order of magnitude to start run
   double m_P = frc::SmartDashboard::GetNumber("Pd", 0.1);
-  positionTotal = frc::SmartDashboard::GetNumber("Total Positon", 6);
+  positionTotal = frc::SmartDashboard::GetNumber("Total Positon", 20);
   m_leftLeadMotor->GetPIDController().SetP(m_P);
   m_rightLeadMotor->GetPIDController().SetP(m_P);
 
@@ -91,8 +91,18 @@ void Robot::AutonomousPeriodic() {
     currentPosition += currentVelocity * timeElapsed;
     
     //convert to rots
-    m_leftLeadMotor->GetPIDController().SetReference(Robot::convertDistanceToTicks(currentPosition), rev::ControlType::kPosition);
-    m_rightLeadMotor->GetPIDController().SetReference(Robot::convertDistanceToTicks(currentPosition), rev::ControlType::kPosition);
+    frc::SmartDashboard::PutNumber("currPos", currentPosition);
+    //frc::SmartDashboard::PutNumber("convertedToRotsPoint", Robot::convertDistanceToTicks(currentPosition));
+    //double inRots = (currentPosition*12) / (3.14 * 5.7) * 42 * ((14/50) * (24/40));
+    double inches = currentPosition * 12;
+    frc::SmartDashboard::PutNumber("inInches", inches);
+    //0.168 is gear ratio
+    double dividebyDia = inches / (3.14 * 5.7) * 42 * (0.168);
+    frc::SmartDashboard::PutNumber("Divide By Dia", dividebyDia);
+    //frc::SmartDashboard::PutNumber("inRots", inRots);
+    double inRots = dividebyDia;
+    m_leftLeadMotor->GetPIDController().SetReference(inRots, rev::ControlType::kPosition);
+    m_rightLeadMotor->GetPIDController().SetReference(inRots, rev::ControlType::kPosition);
 
     prevTime = frc::Timer::GetFPGATimestamp();
   }
