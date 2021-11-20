d5fed103310f204bfe37bad8c8c8bd405246e27a
aditya2604 <aditya26042005@gmail.com>
2021-11-20 11:40:49 -0800

    works; turns are not in place

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -18,10 +18,67 @@ void Robot::RobotInit() {
   m_leftFollowMotor->Follow(*m_leftLeadMotor, false);
   m_rightLeadMotor->SetInverted(false);
   m_rightFollowMotor->Follow(*m_rightLeadMotor, false);
+
+  m_autoChooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_autoChooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  m_yawChooser.SetDefaultOption(kYawDefault, kYawDefault);
+  m_yawChooser.AddOption(kYawXAxis, kYawXAxis);
+  m_yawChooser.AddOption(kYawYAxis, kYawYAxis);
+  frc::SmartDashboard::PutData("Auto Modes", &m_autoChooser);
+  frc::SmartDashboard::PutData("IMUYawAxis", &m_yawChooser);
+  frc::SmartDashboard::PutBoolean("RunCal", false);
+  frc::SmartDashboard::PutBoolean("ConfigCal", false);
+  frc::SmartDashboard::PutBoolean("Reset", false);
+  frc::SmartDashboard::PutBoolean("SetYawAxis", false);
 }
 void Robot::RobotPeriodic() {
   frc::SmartDashboard::PutNumber("left y: ", -(m_stick->GetRawAxis(1)));
   frc::SmartDashboard::PutNumber("right x: ", m_stick->GetRawAxis(4));
+
+  frc::SmartDashboard::PutNumber("YawAngle", m_imu.GetAngle());
+
+  if (m_imu.GetAngle() < 360) {
+      m_robotDrive->ArcadeDrive(0.16, 0.16);
+  }
+  frc::SmartDashboard::PutNumber("XCompAngle", m_imu.GetXComplementaryAngle());
+  frc::SmartDashboard::PutNumber("YCompAngle", m_imu.GetYComplementaryAngle());
+  m_runCal = frc::SmartDashboard::GetBoolean("RunCal", false);
+  m_configCal = frc::SmartDashboard::GetBoolean("ConfigCal", false);
+  m_reset = frc::SmartDashboard::GetBoolean("Reset", false);
+  m_setYawAxis = frc::SmartDashboard::GetBoolean("SetYawAxis", false);
+  m_yawSelected = m_yawChooser.GetSelected();
+
+  // Set IMU settings
+  /*if (m_configCal) {
+    m_imu.ConfigCalTime(8);
+    m_configCal = frc::SmartDashboard::PutBoolean("ConfigCal", false);
+  }
+  if (m_reset) {
+    m_imu.Reset();
+    m_reset = frc::SmartDashboard::PutBoolean("Reset", false);
+  }
+  if (m_runCal) {
+    m_imu.Calibrate();
+    m_runCal = frc::SmartDashboard::PutBoolean("RunCal", false);
+  }*/
+  
+  // Read the desired yaw axis from the dashboard
+  if (m_yawSelected == "X-Axis") {
+    m_yawActiveAxis = frc::ADIS16448_IMU::IMUAxis::kX;
+  }
+  else if (m_yawSelected == "Y-Axis") {
+    m_yawActiveAxis = frc::ADIS16448_IMU::IMUAxis::kY;
+  }
+  else {
+    m_yawActiveAxis = frc::ADIS16448_IMU::IMUAxis::kZ;
+  }
+  // Set the desired yaw axis from the dashboard
+  if (m_setYawAxis) {
+    m_imu.SetYawAxis(m_yawActiveAxis);
+    m_setYawAxis = frc::SmartDashboard::PutBoolean("SetYawAxis", false);
+  }
+
+  
 }
  
 void Robot::AutonomousInit() {}
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -10,6 +10,12 @@
 #include <frc/Spark.h>
 #include "SFDrive.h"
 #include <frc/Solenoid.h>
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+
+#include <adi/ADIS16448_IMU.h>
 
 class Robot : public frc::TimedRobot {
  public:
@@ -40,7 +46,7 @@ class Robot : public frc::TimedRobot {
   float PSI;
   float var_input;
   bool reached_max_pressure = false;
-  bool pressed_button_pressure = true;
+  bool pressed_button_pressure = false;
 
   frc::AnalogInput * analog_input = new frc::AnalogInput(1);
 
@@ -59,5 +65,22 @@ class Robot : public frc::TimedRobot {
   SFDrive* m_robotDrive = new SFDrive(m_leftLeadMotor, m_rightLeadMotor);
 
   frc::Solenoid valve{0};
+
+private:
+  frc::SendableChooser<std::string> m_autoChooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+  frc::ADIS16448_IMU m_imu{};
+  frc::SendableChooser<std::string> m_yawChooser;
+  const std::string kYawDefault = "Z-Axis";
+  const std::string kYawXAxis = "X-Axis";
+  const std::string kYawYAxis = "Y-Axis";
+  std::string m_yawSelected;
+  bool m_runCal = false;
+  bool m_configCal = false;
+  bool m_reset = false;
+  bool m_setYawAxis = false;
+  frc::ADIS16448_IMU::IMUAxis m_yawActiveAxis = frc::ADIS16448_IMU::IMUAxis::kZ;
 
 };
